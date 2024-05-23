e8db884bbd2e942b126fcd8504519873143b6351
ArinaMMa <arinamancinimarlot@gmail.com>
2024-05-23 16:55:38 +0200

    feat(home): add home page and article components

--- assets/js/switchArticle.js
+++ assets/js/switchArticle.js
@@ -4,6 +4,6 @@ document.querySelectorAll('input[data-switch-article-id]')
     .forEach(input => { 
         input.addEventListener('change', async(e) => {
             const id = e.currentTarget.dataset.switchArticleId;
-            sendVisibilityRequest(`/admin/articles./${id}/switch`, e.target);
+            sendVisibilityRequest(`/admin/articles/${id}/switch`, e.target);
         });
     });
